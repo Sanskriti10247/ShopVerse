c93cadae4454b42aad270d679936a407445706a9
Sanskriti <Sanskriti.24bcs10247@sst.scaler.com>
2025-10-22 01:31:54 +0530

    Configured CORS for live frontend

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import http from "http";
 import { Server } from "socket.io";
-
-dotenv.config();
-
-const app = express();
-
-let io;
-
-const server = http.createServer(app);
-io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    credentials: true,
-  },
-});
-
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -26,7 +12,22 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
+dotenv.config();
+
 connectDB();
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://shopverse-frontend-eta.vercel.app", 
+      "http://localhost:5173",                     
+    ],
+    credentials: true,
+  },
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,7 +35,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "https://shopverse-frontend-eta.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
@@ -51,13 +55,17 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("⚡️ Client connected:", socket.id);
-  socket.on("disconnect", () => console.log("❌ Client disconnected:", socket.id));
+
+  socket.on("disconnect", () => {
+    console.log("❌ Client disconnected:", socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 5001;
-server.listen(PORT, () =>
-  console.log(`Server running with Socket.IO on port ${PORT}`)
-);
+server.listen(PORT, () => {
+  console.log(`Server running with Socket.IO on port ${PORT}`);
+});
+
 setInterval(() => {
   io.emit("ping", { time: new Date().toISOString() });
 }, 5000);
